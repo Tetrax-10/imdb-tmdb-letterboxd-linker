04dc833219b0776f492ba8b8dbce22a65c21612c
Raghavan <raghavanmarch2002@gmail.com>
2024-10-12 23:42:02 +0530

    2.1

--- linker.user.js
+++ linker.user.js
@@ -3,7 +3,7 @@
 // @description  Opens the corresponding IMDb, TMDB, or Letterboxd page for movies, TV shows and people with a single click. Additionally, it also displays IMDb ratings on both TMDB and Letterboxd pages.
 // @author       Tetrax-10
 // @namespace    https://github.com/Tetrax-10/imdb-tmdb-letterboxd-linker
-// @version      2.0
+// @version      2.1
 // @license      MIT
 // @match        *://*.imdb.com/title/tt*
 // @match        *://*.imdb.com/name/nm*
